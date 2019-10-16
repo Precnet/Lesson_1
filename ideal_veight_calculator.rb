f458dafb6c6349b458ab1f766dbadaed3fad3fc6
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 20:21:36 +0300

    added constant for weight calculation code moved to functions

--- ideal_veight_calculator.rb
+++ ideal_veight_calculator.rb
@@ -2,8 +2,21 @@
 # Program prints user`s ideal weight computed by expression 'height - 110' by name
 # If ideal weight is less then zero, tell user his weight is already optimal
 
-print "Please, enter your name: "
-user_name = gets().chomp
+WEIGHT_CALCULATION_PENALTY = 110
 
-print "Enter your current weight: "
-user_weight = gets().chomp
+def get_user_name
+  print "Please, enter your name: "
+   return gets().chomp
+end
+
+def get_user_weight
+  print "Enter your current weight: "
+  return gets().chomp
+end
+
+
+
+user_name = get_user_name
+user_weight = get_user_weight
+
+user_optimal_weight = Integer(user_weight) - WEIGHT_CALCULATION_PENALTY
