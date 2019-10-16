8c395a86920458d4a439183f51ef004d2f1b9a39
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 21:13:54 +0300

    added hash for triangle data

--- rectangular_type_determinator.rb
+++ rectangular_type_determinator.rb
@@ -6,3 +6,6 @@ def get_triangle_side(side_number)
   print "Please, enter #{side_number} side of a triangle: "
   return gets.chomp.to_f
 end
+
+triangle_data = Hash.new
+TRIANGLE_SIDES.each {|side| triangle_data[side] = get_triangle_side(side)}
