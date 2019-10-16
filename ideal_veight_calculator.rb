d52aacf078ee7c011c6855b6df875b37399a0017
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 20:07:51 +0300

    added program description

--- ideal_veight_calculator.rb
+++ ideal_veight_calculator.rb
@@ -1,1 +1,5 @@
+# Program requests user`s name and height
+# Program prints user`s ideal weight computed by expression 'height - 110' by name
+# If ideal weight is less then zero, tell user his weight is already optimal
+
 
