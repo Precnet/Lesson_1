3c4ee7de72036d9b6216b3e6bec72c9259082ae0
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 20:52:35 +0300

    added function for t height request

--- triangle_area_calculator.rb
+++ triangle_area_calculator.rb
@@ -6,3 +6,8 @@ def get_triangle_base
   print "Please, enter triangle base: "
   return gets.chomp.to_i
 end
+
+def get_triangle_height
+  print "Please, enter triangle height: "
+  return gets.chomp.to_i
+end
