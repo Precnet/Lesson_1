02a37b40567fad6c40a8c3b5cc4128395f604d3c
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 20:13:30 +0300

    added basics of name check

--- ideal_veight_calculator.rb
+++ ideal_veight_calculator.rb
@@ -2,4 +2,10 @@
 # Program prints user`s ideal weight computed by expression 'height - 110' by name
 # If ideal weight is less then zero, tell user his weight is already optimal
 
+print "Please, enter your name: "
 user_name = gets().chomp
+
+if !user_name.match(/^[[:alpha:][:blank:]]+$/)
+  print "Probably you enter a wrong name. Would you like to enter new name?(y/n) "
+  wrong_name_alert_reply = gets().chomp
+end
