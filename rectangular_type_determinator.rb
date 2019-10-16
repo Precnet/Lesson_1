3c3957753c9aa23a3e83bb4eff9ab74c0e296941
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 21:10:01 +0300

    added constant to iterate over triangle sides

--- rectangular_type_determinator.rb
+++ rectangular_type_determinator.rb
@@ -1,5 +1,7 @@
 # Program asks for three sides of a triangle and output its type
 
+TRIANGLE_SIDES = %w(first second third)
+
 def get_triangle_side(side_number)
   print "Please, enter #{side_number} side of a triangle: "
   return gets.chomp.to_f
