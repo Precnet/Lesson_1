4889d87bacd19b6e51cdb1f6b91329cedd20916c
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 21:07:05 +0300

    added function to request a side

--- rectangular_type_determinator.rb
+++ rectangular_type_determinator.rb
@@ -1,1 +1,6 @@
 # Program asks for three sides of a triangle and output its type
+
+def get_triangle_side(side_number)
+  print "Please, enter #{side_number} side of a triangle: "
+  return gets.chomp.to_f
+end
