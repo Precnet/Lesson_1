b1b8a5e19f8c0d5a672d2e64aa01e1c2b0a6a63f
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 20:10:43 +0300

    get input from user

--- ideal_veight_calculator.rb
+++ ideal_veight_calculator.rb
@@ -2,4 +2,4 @@
 # Program prints user`s ideal weight computed by expression 'height - 110' by name
 # If ideal weight is less then zero, tell user his weight is already optimal
 
-
+user_name = gets().chomp
