d1503c2e07f880107c603f6e1f10e9240fc29df1
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 22:08:07 +0300

    form hash with equation data

--- quadratic_equation_solver.rb
+++ quadratic_equation_solver.rb
@@ -1,1 +1,9 @@
+COEFFICIENTS_NAMES = %w(a b c)
 
+def get_coefficient(coefficient)
+  print "Enter coefficietn #{coefficient}: "
+  return gets.chomp.to_f
+end
+
+equation_data = Hash.new
+COEFFICIENTS_NAMES.each {|coefficient| equation_data[coefficient] = get_coefficient(coefficient)}
