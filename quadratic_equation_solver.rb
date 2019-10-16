38f6cc651896745dfa4e6bd813485e2e4c7390aa
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 22:47:49 +0300

    final changes

--- quadratic_equation_solver.rb
+++ quadratic_equation_solver.rb
@@ -22,22 +22,29 @@ def calculate_discriminant(equation_data)
 end
 
 def calculate_roots(equation)
-  equation_discriminant = calculate_discriminant(equation)
-  a = equation_data["a"]
-  b = equation_data["b"]
-  c = equation_data["c"]
+  # method doing more than one thing, but I`m kinda tired to rewrite it right now
+  discriminant = calculate_discriminant(equation).to_f
+  a = equation["a"]
+  b = equation["b"]
+  c = equation["c"]
   case
   when discriminant > 0
-
+    x_1 = calculate_root(a, b, Math.sqrt(discriminant))
+    x_2 = calculate_root(a, b, -Math.sqrt(discriminant))
+    return "Discriminant is #{discriminant}. X1 is #{x_1}, X2 is #{x_2}"
   when discriminant == 0
-
+    x = calculate_root(a, b, discriminant)
+    return "Discriminant is #{discriminant}. X1 = X2 = #{x}"
   else
-
+    return "Discriminant is #{discriminant}. There are no roots"
   end
 end
 
-def calculate
+def calculate_root(a, b, root_of_d)
+  return (-b + root_of_d) / (2 * a)
+end
 
 equation_data = Hash.new
 COEFFICIENTS_NAMES.each {|coefficient| equation_data[coefficient] = get_coefficient(coefficient)}
 print_equation(equation_data)
+print(calculate_roots(equation_data))
