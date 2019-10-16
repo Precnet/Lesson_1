8a99713f0643c13b3d7b10643c9b0c3134591e71
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-16 22:12:22 +0300

    added function to calculate discriminant

--- quadratic_equation_solver.rb
+++ quadratic_equation_solver.rb
@@ -1,9 +1,15 @@
 COEFFICIENTS_NAMES = %w(a b c)
 
 def get_coefficient(coefficient)
-  print "Enter coefficietn #{coefficient}: "
+  print "Enter coefficient #{coefficient}: "
   return gets.chomp.to_f
+end
+
+def calculate_discriminant(equation_data)
+  return equation_data["b"]**2 - 4 * equation_data["a"] * equation_data["c"]
 end
 
 equation_data = Hash.new
 COEFFICIENTS_NAMES.each {|coefficient| equation_data[coefficient] = get_coefficient(coefficient)}
+
+print "#{equation_data["a"]}x**2 + #{equation_data["b"]}x + #{equation_data["c"]}"
