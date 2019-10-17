4767fd158e0ec47a0b9979d0450a7dc1d4f696ba
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-17 07:59:54 +0300

    removed unnecessary returns but I dont think I like it this way It looks obscured now

--- quadratic_equation_solver.rb
+++ quadratic_equation_solver.rb
@@ -2,7 +2,7 @@ COEFFICIENTS_NAMES = %w(a b c)
 
 def get_coefficient(coefficient)
   print "Enter coefficient #{coefficient}: "
-  return gets.chomp.to_f
+  gets.chomp.to_f
 end
 
 def print_equation(equation_data)
@@ -14,11 +14,11 @@ def print_equation(equation_data)
 end
 
 def get_number_readable_form(number)
-  return number > 0 ? "+ #{number}" : "- #{number * -1}"
+  number > 0 ? "+ #{number}" : "- #{number * -1}"
 end
 
 def calculate_discriminant(equation_data)
-  return equation_data["b"]**2 - 4 * equation_data["a"] * equation_data["c"]
+  equation_data["b"]**2 - 4 * equation_data["a"] * equation_data["c"]
 end
 
 def calculate_roots(equation)
@@ -31,17 +31,18 @@ def calculate_roots(equation)
   when discriminant > 0
     x_1 = calculate_root(a, b, Math.sqrt(discriminant))
     x_2 = calculate_root(a, b, -Math.sqrt(discriminant))
-    return "Discriminant is #{discriminant}. X1 is #{x_1}, X2 is #{x_2}"
+    equation_verdict_message = "Discriminant is #{discriminant}. X1 is #{x_1}, X2 is #{x_2}"
   when discriminant == 0
     x = calculate_root(a, b, discriminant)
-    return "Discriminant is #{discriminant}. X1 = X2 = #{x}"
+    equation_verdict_message = "Discriminant is #{discriminant}. X1 = X2 = #{x}"
   else
-    return "Discriminant is #{discriminant}. There are no roots"
+    equation_verdict_message = "Discriminant is #{discriminant}. There are no roots"
   end
+  equation_verdict_message
 end
 
 def calculate_root(a, b, root_of_d)
-  return (-b + root_of_d) / (2 * a)
+  (-b + root_of_d) / (2 * a)
 end
 
 equation_data = Hash.new
